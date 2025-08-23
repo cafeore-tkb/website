6b09b2bdc45ff9a9999623f8232f14e2195d135a
toririm <longtimedragon@icloud.com>
2025-08-23 22:35:54 +0900

    use microcms

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,12 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {}
 }
+
+interface ImportMetaEnv {
+	readonly MICROCMS_API_URL: string;
+	readonly MICROCMS_API_KEY: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
