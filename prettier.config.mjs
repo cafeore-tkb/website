cf12a0d9e1b90350dd72af1863d9a67d318a3776
toririm <longtimedragon@icloud.com>
2025-08-28 11:58:20 +0900

    format

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,15 +5,15 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  "plugins": ["prettier-plugin-astro"],
-  "overrides": [
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
     {
-      "files": "*.astro",
-      "options": {
-        "parser": "astro"
-      }
-    }
-  ]
-}
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
 
 export default config;
