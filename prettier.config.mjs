4e17df4ad63161b55cf257d95444c44c8f657aed
toririm <longtimedragon@icloud.com>
2025-08-28 11:44:15 +0900

    Add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+// ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  "plugins": ["prettier-plugin-astro"],
+  "overrides": [
+    {
+      "files": "*.astro",
+      "options": {
+        "parser": "astro"
+      }
+    }
+  ]
+}
+
+export default config;
