c8f5731501b1c07e24ad32c3e0584335f371d885
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-08-31 22:15:20 +0900

    珈琲・俺ロゴやハンバーガーロゴを追加・アストロロゴを削除

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,13 @@
+export default function HamburgerMenu({ className = "" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      fill="currentColor"
+      className={className}
+      aria-hidden="true"
+    >
+      <path d="M120-693.33V-760h720v66.67H120ZM120-200v-66.67h720V-200H120Zm0-246.67v-66.66h720v66.66H120Z" />
+    </svg>
+  );
+}
