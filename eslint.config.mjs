d7fa54d88979ac966903aa00eca3d35efd91a8e4
toririm <longtimedragon@icloud.com>
2025-09-15 01:13:04 +0900

    eslint: Add ignore files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // ts-check
 
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default defineConfig([
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  globalIgnores(["dist/", "node_modules/"]),
 ]);
