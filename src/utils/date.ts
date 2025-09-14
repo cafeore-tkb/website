eda0fbd5f1dcc0dd7ab8346827ff6a4a94e4b245
toririm <longtimedragon@icloud.com>
2025-09-15 06:27:29 +0900

    tweak: Change date format hour numeric

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,7 +3,7 @@ export const formatDate = (date: Date) => {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
-    hour: "2-digit",
+    hour: "numeric",
     minute: "2-digit",
     timeZone: "Asia/Tokyo",
   });
