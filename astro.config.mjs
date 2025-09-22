e2452b27f9ce01bf0b714fcd6449912063714d9a
toririm <longtimedragon@icloud.com>
2025-09-22 23:10:18 +0900

    Fix: Astro.site setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cafeore.cafe",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
